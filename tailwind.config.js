aa3e6724fcb3ab178d7f29f8c5aecdd099fb8169
Rahul <146958652+Rahul-Rathour@users.noreply.github.com>
2025-11-20 14:50:57 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,40 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+
   theme: {
-    extend: {},
+    extend: {
+      transformStyle: {
+        'preserve-3d': 'preserve-3d',
+      },
+      backfaceVisibility: {
+        'hidden': 'hidden',
+      },
+      rotate: {
+        'y-180': '180deg',
+      },
+      perspective: {
+        '1000': '1000px',
+      },
+    },
   },
-  plugins: [],
-}
+
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.transform-style-preserve-3d': {
+          transformStyle: 'preserve-3d',
+        },
+        '.backface-hidden': {
+          backfaceVisibility: 'hidden',
+        },
+        '.rotate-y-180': {
+          transform: 'rotateY(180deg)',
+        },
+        '.perspective-1000': {
+          perspective: '1000px',
+        },
+      });
+    },
+  ],
+};
